41470b8188ba6747943422ca4a4c60fb4e0823e8
Dreta <32196824+Dreta@users.noreply.github.com>
2024-06-17 07:20:33 +0000

    solve "apple division"

--- bronze/week4/apple-division.cpp
+++ bronze/week4/apple-division.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+int n;
+vector<long long> p;
+
+long long solve(long long group1Sum, long long group2Sum, long long index) {
+    if (index == n) {
+        return abs(group2Sum - group1Sum);
+    }
+    return min(
+        solve(group1Sum + p[index], group2Sum, index + 1),
+        solve(group1Sum, group2Sum + p[index], index + 1)
+    );
+}
+
 int main(void) {
-    cout << "Hello, world!" << endl;
+    cin >> n;
+    p.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+
+    cout << solve(0, 0, 0) << endl;
 }

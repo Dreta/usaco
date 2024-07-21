22208a3ba55563effa2c398362fd6536f5aac5b0
Dreta <32196824+Dreta@users.noreply.github.com>
2024-07-21 07:28:37 +0000

    solve "sleepy cow sorting"

---
+++ bronze/week-6/sleepy-cow-sorting.cpp
@@ -0,0 +1,28 @@
+// USACO 2019 January Contest, Bronze
+// Problem 2. Sleepy Cow Sorting
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    freopen("sleepy.in", "r", stdin);
+    freopen("sleepy.out", "w", stdout);
+
+    int N;
+
+    cin >> N;
+    int p[N];
+    for (int i = 0; i < N; i++) {
+        cin >> p[i];
+    }
+
+    int y = 1;
+    for (int i = N - 1; i >= 1; i--) {
+        if (p[i - 1] < p[i]) {
+            y++;
+        } else {
+            break;
+        }
+    }
+    cout << N - y << endl;
+}

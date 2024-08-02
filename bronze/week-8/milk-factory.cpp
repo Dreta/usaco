147db1c1d1e45707a0b78095954fbaf566ff3595
Dreta <32196824+Dreta@users.noreply.github.com>
2024-08-02 07:15:48 +0000

    solve "milk factory"

--- bronze/week-8/milk-factory.cpp
+++ bronze/week-8/milk-factory.cpp
@@ -1,47 +1,25 @@
+// USACO 2019 US Open Contest, Bronze
+// Problem 2. Milk Factory
+
+#include <iostream>
 #include <vector>
 using namespace std;
-
-class UndirectedGraph {
-public:
-    int vertices;
-    vector<vector<int>> adj;
-
-    UndirectedGraph(int vertices) {
-        this->vertices = vertices;
-        this->adj = vector<vector<int>>(vertices);
-    }
-
-    void addEdge(int u, int v) {
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    vector<int> adjacent(int u) {
-        return adj[u];
-    }
-
-    int edges() {
-        int edges = 0;
-        for (int i = 0; i < vertices; i++)
-        {
-            edges += adj[i].size();
-        }
-        return edges / 2;
-    }
-};
 
 class DirectedGraph {
 public:
     int vertices;
     vector<vector<int>> adj;
+    vector<vector<int>> adjInv; // inverted adj, for quick query purposes
 
     DirectedGraph(int vertices) {
         this->vertices = vertices;
         this->adj = vector<vector<int>>(vertices);
+        this->adjInv = vector<vector<int>>(vertices);
     }
 
     void addEdge(int u, int v) {
         adj[u].push_back(v);
+        adjInv[v].push_back(u);
     }
 
     vector<int> adjacent(int u) {
@@ -84,3 +62,39 @@ class DepthFirstTraversal {
         return marked[vertex];
     }
 };
+
+int main() {
+    int N;
+    cin >> N;
+
+    DirectedGraph graph(N);
+    for (int i = 0; i < N - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph.addEdge(a - 1, b - 1);
+    }
+
+    for (int vertex = 0; vertex < N; vertex++) {
+        if (graph.adjInv[vertex].size() == 0) { // If there are no incoming edges, this is not the answer.
+            continue;
+        }
+        bool flag = true;
+        for (int v = 0; v < N; v++) {
+            if (v == vertex) {
+                continue;
+            }
+            DepthFirstTraversal dfs(graph, v);
+            if (!dfs.hasPathTo(vertex)) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            cout << vertex + 1 << endl;
+            return 0;
+        }
+    }
+
+
+    cout << -1 << endl;
+}
